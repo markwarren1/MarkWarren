1aec52a10ba618d903faab8bf6b70415e4782ec8
Zebulacious <markwarren@valdosta.edu>
2022-11-11 12:57:36 -0500

    worked on buttons

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,23 @@
+//toggling between social media views
 function toggle(e){
     const button = e.target;
     const divId = button.dataset.targetId;
     const div = document.getElementById(divId);
     const boxes = document.querySelectorAll('.toggle')
 
-    boxes.forEach(box => {
-        box.classList.remove("d-block")
-    })
-    // div.classList.remove("d-none");
-    div.classList.add("d-block") 
+    if(div.classList.contains("d-block")) { return }
+        boxes.forEach(box => {
+            box.classList.remove("d-block")
+        })
+        // div.classList.remove("d-none");
+        div.classList.add("d-block") 
 }
 
 //to get instagram (api call)
 var userFeed = new Instafeed({
     get: 'user',
-    target: "instafeed-container",
-    resolution: 'low_resolution',
+    target: "div2",
+    resolution: 'high_resolution',
     accessToken: 'YOUR_INSTAGRAM_ACCESS_TOKEN_GOES_HERE'
 });
 userFeed.run();
