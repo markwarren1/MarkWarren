383866eb94bfd15c2191397b8ff833f3c16587dd
Mark <mark16810@gmail.com>
2023-05-20 11:06:00 -0400

    Improved header buttons and cleaned up JS

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,6 @@
 require('dotenv').config();
 
 //toggling between social media views
-
 function handleButtonClick(event) {
       var targetId = event.target.getAttribute("data-target-id");
       var contentElements = document.getElementsByClassName("content");
@@ -19,46 +18,6 @@ function handleButtonClick(event) {
       document.getElementById(targetId).classList.add("active");
       event.target.classList.add("btn-primary");
 
-      if (targetId === "div2") {
-        // Fetch and display Instagram content
-        fetchInstagramContent();
-      }
-    }
-
-    function fetchInstagramContent() {
-      axios
-        .get("https://api.example.com/instagram")
-        .then(function (response) {
-          var instafeedContainer = document.getElementsByClassName("instafeed-container")[0];
-          instafeedContainer.innerHTML = response.data;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
-
-    function fetchInstagramContent() {
-      axios
-        .get("https://api.example.com/instagram")
-        .then(function (response) {
-          var instafeedContainer = document.getElementsByClassName("instafeed-container")[0];
-          instafeedContainer.innerHTML = response.data;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
-
-function fetchInstagramContent() {
-      axios
-        .get("https://api.example.com/instagram")
-        .then(function (response) {
-          var instafeedContainer = document.getElementsByClassName("instafeed-container")[0];
-          instafeedContainer.innerHTML = response.data;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
     }
 
 
