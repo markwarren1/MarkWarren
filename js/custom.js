6df79f4e00026b8d015d213461f4b437103c61bb
Mark <mark16810@gmail.com>
2023-06-14 19:31:23 -0400

    projects.html button update and resume pdf update

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 
 
-//toggling between social media views
+//toggling between social media views on the contact page
 function handleButtonClick(event) {
       var targetId = event.target.getAttribute("data-target-id");
       var contentElements = document.getElementsByClassName("content");
@@ -18,3 +18,35 @@ function handleButtonClick(event) {
       event.target.classList.add("btn-primary");
 
     }
+
+//get most recent commit time in format mm/dd/yyyy of main branch using github api
+//FOR PERSONAL WEBPAGE
+function getCommitTime1() {
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://api.github.com/repos/markwarren1/MarkWarren/commits/main', true);
+  request.onload = function () {
+    var data = JSON.parse(this.response);
+    var date = new Date(data.commit.committer.date);
+    var formattedDate = date.toLocaleDateString();
+    //append formatted date to single element with id 'personalWebpage'
+    document.getElementById('personalWebpage').innerHTML += formattedDate;
+  }
+  request.send();
+}
+
+//get most recent commit time in format mm/dd/yyyy of main branch using github api
+//FOR Tic-Tac-Toe
+function getCommitTime2() {
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://api.github.com/repos/markwarren1/Tic-Tac-ToeGame/commits/master', true);
+  request.onload = function () {
+    var data = JSON.parse(this.response);
+    var date = new Date(data.commit.committer.date);
+    var formattedDate = date.toLocaleDateString();
+    //append formatted date to single element with id 'personalWebpage'
+    document.getElementById('tictactoe').innerHTML += formattedDate;
+  }
+  request.send();
+}
+    
+    
